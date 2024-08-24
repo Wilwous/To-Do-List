823501a7d6f26ae55299af9defceda7fdc842660
Wilwous <mick.thomson.tm@gmail.com>
2024-08-24 13:02:03 +0300

    Initial Commit

---
+++ To-Do List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do List
+//
+//  Created by Антон Павлов on 24.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
